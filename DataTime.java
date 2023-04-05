036255428f4ae4a7806e19407d426a0ca53b7616
OLENUA <71031846+OLENUA@users.noreply.github.com>
2023-04-05 13:17:55 +0300

    Add files via upload

--- DataTime.java
+++ DataTime.java
@@ -1,19 +1,33 @@
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class DataTime {
-    public DataTime(String surName, Date birthDate) {
+    public DataTime(String surName, Date birthDate) throws ParseException {
+
         long mills = Long.MAX_VALUE;
         Date result = null;
+
+
         ArrayList<Date> dateArrayList = new ArrayList<>();
-        Date date0 = new Date(2200, 8, 1);
-        Date date1 = new Date(2000, 7, 15);
-        Date date2 = new Date(2212, 11, 16);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy,MM,dd");
+        String dateInString0 = "2004,2,10";
+        Date date0 = formatter.parse(dateInString0);
         dateArrayList.add(date0);
+
+        String dateInString1 = "2021,2,11";
+        Date date1 = formatter.parse(dateInString1);
         dateArrayList.add(date1);
+
+        String dateInString2 = "2012,4,30";
+        Date date2 = formatter.parse(dateInString2);
         dateArrayList.add(date2);
+
+        String dateInString3 = "2000,1,1";
+        Date date3 = formatter.parse(dateInString3);
+        dateArrayList.add(date3);
 
         for (int i = 0; i < dateArrayList.size(); i++) {
             long delta = Math.abs(birthDate.getTime() - dateArrayList.get(i).getTime());
@@ -22,14 +36,16 @@ public class DataTime {
                 mills = delta;
             }
         }
-
         SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy,MM,dd");
         String dante = DATE_FORMAT.format(result);
         System.out.println(dante);
-        }
-        public void ageCalculate(String surName,Date birthDate){
 
-            Date ourDays = new Date(2023,4,4);
+        }
+        public void ageCalculate(String surName,Date birthDate) throws ParseException {
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy,MM,dd");
+            String dateInString0 = "2023,4,5";
+            Date ourDays = formatter.parse(dateInString0);
             int ageDeltaY = Math.abs(ourDays.getYear() - birthDate.getYear());
             int ageDeltaM = Math.abs(ourDays.getMonth() - birthDate.getMonth());
             if (ageDeltaM > ourDays.getMonth()) {ageDeltaY = ageDeltaY-1 ;}
--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         String surName = "lutskin";
-        Date birthDate = new Date(2004,9,10);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy,MM,dd");
+        String birth = "2004,9,10";
+        Date birthDate = formatter.parse(birth);
         DataTime dt = new DataTime(surName,birthDate);
         dt.ageCalculate("Alex",birthDate);
 
